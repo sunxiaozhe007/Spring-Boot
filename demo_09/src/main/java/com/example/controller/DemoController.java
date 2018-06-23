06f292641f79eec285f13c29234e93db6abb5dca
sunxiaozhe007 <985160269@qq.com>
2018-06-23 15:03:17 +0800

    Thymeleaf基本使用

---
+++ demo_09/src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Thymeleaf入门案例
+ */
+@Controller
+public class DemoController {
+
+    @RequestMapping("/show")
+    public String showInfo(Model model){
+        model.addAttribute("msg","Thymeleaf第一个案例");
+        return "index";
+    }
+}

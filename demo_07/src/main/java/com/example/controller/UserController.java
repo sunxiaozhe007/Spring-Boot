22717a98e1cb1fe151c4230bbe7cd200bdeec1af
sunxiaozhe007 <985160269@qq.com>
2018-06-25 17:45:41 +0800

    springBoot整合jsp

---
+++ demo_07/src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import com.example.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * SpringBoot整合jsp
+ */
+@Controller
+public class UserController {
+
+    /**
+     * 处理请求，产生数据
+     */
+    @RequestMapping("/showUser")
+    public String showUser(Model model){
+        List<User> list = new ArrayList<User>();
+        list.add(new User(1,"孙",24));
+        list.add(new User(2,"晓",23));
+        list.add(new User(3,"哲",22));
+        list.add(new User(4,"嗯",21));
+
+        //需要一个Model对象
+        model.addAttribute("list",list);
+        //跳转视图
+        return "userList";
+
+    }
+}
---
+++ demo_07/src/main/java/com/example/domain/User.java
@@ -0,0 +1,40 @@
+package com.example.domain;
+
+public class User {
+    private Integer userid;
+    private String username;
+    private Integer userage;
+
+    public User() {
+    }
+
+    public User(Integer userid, String username, Integer userage) {
+        this.userid = userid;
+        this.username = username;
+        this.userage = userage;
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getUserage() {
+        return userage;
+    }
+
+    public void setUserage(Integer userage) {
+        this.userage = userage;
+    }
+}

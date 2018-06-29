8f13d5c6f2abf5b6f9b7537a7c77f80d9c7a44ee
sunxiaozhe007 <985160269@qq.com>
2018-06-29 18:18:11 +0800

    SpringBoot异常处理-@ExceptionHandler

---
+++ demo_15/src/main/java/example/controller/DemoController.java
@@ -0,0 +1,25 @@
+package example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DemoController {
+
+    @RequestMapping("/show")
+    public String showInfo(){
+        String str = null;
+        str.length();
+        return "index";
+    }
+
+
+    @RequestMapping("/show2")
+    public String showInfo2(){
+        int a = 10/0;
+        return "index";
+    }
+
+}

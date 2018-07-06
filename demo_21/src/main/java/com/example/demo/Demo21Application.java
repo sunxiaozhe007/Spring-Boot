b13d3de9b7dad3ccc6363c0c5e05abafeb51976b
sunxiaozhe007 <985160269@qq.com>
2018-07-06 19:54:46 +0800

    启动器

---
+++ demo_21/src/main/java/com/example/demo/Demo21Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+@SpringBootApplication
+public class Demo21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo21Application.class, args);
+    }
+}

f163459c411f507642cc97ec0c3e8f7ac458b77a
sunxiaozhe007 <985160269@qq.com>
2018-07-14 14:01:41 +0800

    Spring Boot定时任务-Scheduled

---
+++ demo_24/src/test/java/com/example/demo_24/Demo24ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demo_24;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo24ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
